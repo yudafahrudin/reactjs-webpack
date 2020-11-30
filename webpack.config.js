57eeb8b45058539cabb0a6694c70d7889e088c5d
yudafahrudin <yuda@vontis.id>
2020-12-01 01:36:38 +0700

    Initialize react project

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        writeToDisk: true,
+        compress: true,
+        port: 8000,
+        open: true
+    },
+    output: {
+        filename: '[name].[contenthash].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: './',
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            { test: /\.txt$/, use: 'raw-loader' },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env', '@babel/preset-react'
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties',
+                            '@babel/plugin-syntax-dynamic-import'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            // include all types of chunks
+            chunks: 'all'
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            'React': 'react',
+            'ReactDOM': 'react-dom'
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html',
+            favicon: './public/favicon.ico',
+            manifest: './public/manifest.json'
+        }),
+        new CleanWebpackPlugin(),
+    ]
+};
